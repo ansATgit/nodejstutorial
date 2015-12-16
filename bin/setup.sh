158f7e075b9a204f8b22e9b86dd2b1fd46a399ec
Anshul Ranjan <anshulr@amazon.com>
2015-12-16 11:44:44 +0530

    Fixed setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,9 +4,9 @@
 
 # Install supervisor, express
 sudo npm install -g supervisor
-sudo npm installed -g express
+sudo npm install -g express
 
 # Install dep locally
 npm install
 # Running server
-supervisor -w '.,views,routes' -e 'nunj' bin/www
+supervisor -w '.,views,routes' -e 'js,nunj' bin/www
